d0fde62df9097ba0e3e6002f0c4317e037798eb5
HayberthSepu <hsepulveda@abatechnology.com>
2024-08-26 15:57:23 -0500

    Lo que hicimos fue borrar un archivo

--- test/application_system_test_case.rb
+++
@@ -1,6 +0,0 @@
-require "test_helper"
-
-class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # Configura el navegador que se usará para las pruebas de sistema.
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-end
