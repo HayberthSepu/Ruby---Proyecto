e58f6521f59ff6fc29ebd6967cb2c3b11df3fb98
HayberthSepu <hsepulveda@abatechnology.com>
2024-09-09 15:52:08 -0500

    FEAT(AC-19999)-Add remove in the problem in the proyect

--- test/system/example_test.rb
+++
@@ -1,8 +0,0 @@
-require "application_system_test_case"
-
-class ExampleTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit root_url
-    assert_selector "h1", text: "Welcome"
-  end
-end
